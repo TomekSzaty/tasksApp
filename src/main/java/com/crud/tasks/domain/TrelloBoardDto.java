f0ffe6ab934da5a69156d611babf3666ed280284
TomekSzaty <75204970+TomekSzaty@users.noreply.github.com>
2021-05-28 10:30:06 +0200

    Zadanie: ułożenie kodu

---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -0,0 +1,17 @@
+package com.crud.tasks.domain;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TrelloBoardDto {
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("id")
+    private String id;
+}
