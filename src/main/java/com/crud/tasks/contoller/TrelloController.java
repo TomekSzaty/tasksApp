e1f1489496e10b24af25073649b51dd937bacdad
TomekSzaty <75204970+TomekSzaty@users.noreply.github.com>
2021-05-31 10:04:58 +0200

    22.4. Przygotowania endpointa do eksportowania zadań Zadanie: Piszemy test.

--- src/main/java/com/crud/tasks/contoller/TrelloController.java
+++ src/main/java/com/crud/tasks/contoller/TrelloController.java
@@ -17,24 +17,11 @@ public class TrelloController {
     private final TrelloClient trelloClient;
 
     @GetMapping("getTrelloBoards")
-    public void getTrelloBoards() {
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
-        trelloBoards.stream()
-                .filter(trelloBoardDto -> trelloBoardDto.getId() != null)
-                .filter(trelloBoardDto -> trelloBoardDto.getName() != null)
-                //.filter(trelloBoardDto -> trelloBoardDto.getName().contains("Kodilla"))
-                .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getName() + " " + trelloBoardDto.getId()));
-
-        trelloBoards.forEach(trelloBoardDto -> {
-            System.out.println();//(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
-            System.out.println("This board contains lists: ");
-            trelloBoardDto.getLists().forEach(trelloList -> {
-                System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
-            });
-        });
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloClient.getTrelloBoards();
     }
+
+
     @PostMapping("createTrelloCard")
     public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
         return trelloClient.createNewCard(trelloCardDto);
