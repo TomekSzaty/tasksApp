11747d1730ca9773e275af137069fcdc3290c7e5
TomekSzaty <75204970+TomekSzaty@users.noreply.github.com>
2021-06-01 20:10:20 +0200

    23.2. Instalacja Swaggera,, czyli dokumentacja dla API Zadanie: ograniczenie skanowania w Swaggerze

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -6,11 +6,26 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
-
+@EnableSwagger2
 @Configuration
 public class CoreConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                //.apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
 
     @Bean
     public RestTemplate restTemplate() {
